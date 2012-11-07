54c5bc3a34041ff5369c9134518c6f4fc7ffef13
rohan <rohans@ischool.berkeley.edu>
2012-11-07 01:13:19 -0800

    display tags and freq for 2 words

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+var hash = []; //get the hashtags for each tweet
+var hash1 = []; //get the hashtags for each tweet
+var tags;
+var hashtxt = []; //an array of all the hashtags related to a particular search query
+var hashtxt1 = []; //an array of all the hashtags related to a particular search query
 
+var tagArray = []; //an array of hashtags (without duplicates) related to a particular search query
+var tagArray1 = []; //an array of hashtags (without duplicates) related to a particular search query
+var tagFreq = {}; //Key value pairs. hashtag is the key and frequency is the value
+var tagFreq1 = {}; //Key value pairs. hashtag is the key and frequency is the value
 
 $(function() {
 
@@ -9,7 +18,9 @@ $(function() {
 
 
     // function to get the tweets of the query tag
-    function getTweets(query) {
+    function getTweets(query,input) {
+
+       alert("input is"+input);
 
         $('#tags').fadeTo('slow', 0, function(){  
             //tweets URL
@@ -21,7 +32,11 @@ $(function() {
             $.getJSON(tweetURL, function(json){
                 tweets = json.results;
                 
-                $('#tags').empty();
+                if(input==0){
+                    $('#tags').empty();}
+                if(input==1){
+                    $('#tags1').empty();}
+
                 //check to see if we get a response
                 if(tweets.length == 0){
                     $('#query').append(query + ' does not have any tweets.');
@@ -33,17 +48,17 @@ $(function() {
                     $('#query').prepend(query);
                     $('#relatedTags').show();
 
-                    var hash = []; //get the hashtags for each tweet
+
                  
                     for(var i = 0; i < tweets.length; i++)
                     {
-                        var tags = tweets[i].text;
+                        tags = tweets[i].text;
                         hash.push(tweets[i].entities.hashtags);
                 
                         //$('#tags').append('<p>'+ tags + '</p>')
                     }
 
-                    var hashtxt = []; //an array of all the hashtags related to a particular search query
+                    
 
                     for (var j=0; j < hash.length; j++)
                     {
@@ -75,7 +90,20 @@ $(function() {
                 var z;
                 for (z in tagFreq)
                 {
-                    $('#tags').append('<p>'+ z + ' (' + tagFreq[z] + ')' + '</p>')
+                    if(input==0)
+
+                        {
+                            $('#tags').append('<p>'+ z + ' (' + tagFreq[z] + ')' + '</p>');
+                        }
+
+                    if(input==1)
+                    {
+                            $('#tags1').append('<p>'+ z + ' (' + tagFreq[z] + ')' + '</p>');
+                    }
+
+             //    alert("here");
+
+
                 }
                 
                 $('#tags').fadeTo('slow', 1);
@@ -94,6 +122,7 @@ $(function() {
     // Function for when user submits search form
     $('#searchForm').on('submit', function(e){
     
+        alert("form0");
         e.preventDefault();
         
         //hides keyboard in iOS after hitting go
@@ -102,9 +131,25 @@ $(function() {
         $('#relatedTags').hide();
         var query = $('#searchField').val();
         query = query.toLowerCase();            
-        getTweets(query);
+        getTweets(query,0);
 
     });
+
+    $('#searchForm1').on('submit', function(e){
+    
+        alert("form1");
+        e.preventDefault();
+        
+        //hides keyboard in iOS after hitting go
+        $('#searchField').blur();
+        $('#query').empty();
+        $('#relatedTags').hide();
+        var query = $('#searchField1').val();
+        query = query.toLowerCase();            
+        getTweets(query,1);
+
+    });
+
 
     // Function for when user clicks on a term in #synonyms or #antonyms
     // or in #suggestions list
