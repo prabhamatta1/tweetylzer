292e2ea5dd39aa3e4fc40fe94ed76735dec58875
Haroon Rasheed <haroon@ischool.berkeley.edu>
2012-11-10 08:04:25 -0800

    Bubble Viz

---
+++ Bubble/index.js
@@ -0,0 +1,391 @@
+var data = {};
+
+(function() {
+
+data.parties = [
+];
+
+data.topics = [
+].map(topic);
+
+data.party = function(name, text){
+  var p = {name: name, speeches: text, id: data.parties.length};
+  data.parties.push(p);
+  return p;
+}
+
+data.topic = function(name) {
+  var t = topic({name: name}, data.topics.length);
+  data.topics.push(t);
+  return t;
+};
+
+function topic(topic, i) {
+  topic.id = i;
+  topic.count = 0;
+  topic.cx = topic.x;
+  topic.cy = topic.y;
+
+  topic.parties = data.parties.map(function(party) {
+    var count = 0;
+
+    var text = party.speeches;
+      
+    count = text.split(topic.name).length - 1;
+
+    topic.count += count;
+    return {count: count};
+  });
+
+  return topic;
+}
+
+})();
+
+(function() {
+
+var width = 970,
+    height = 540;
+
+var collisionPadding = 4,
+    clipPadding = 4,
+    minRadius = 16, // minimum collision radius
+    maxRadius = 65, // also determines collision search radius
+    activeTopic; // currently-displayed topic
+
+var formatShortCount = d3.format(",.0f"),
+    formatLongCount = d3.format(".1f"),
+    formatCount = function(d) { return (d < 10 ? formatLongCount : formatShortCount)(d); };
+
+taglist1 = ["fun", "fun", "fun", "fun", "fun", "fun", "rain", "rain", "good", "fun", "fun", "fun", "good", "good", "rain", "one", "one", "one"];
+taglist2 = ["fun", "fun", "rain", "rain", "rain", "fun", "fun", "fun", "rain", "rain", "rain", "rain", "rain", "fun", "fun", "good", "good", "rain", "two", "two"];
+tagAll = ["one", "two", "fun", "good", "rain"];
+tagStr1 = "";
+tagStr2 = "";
+
+for (var i=0; i < taglist1.length; i++)
+{
+  tagStr1 = tagStr1 + " " + taglist1[i];
+}
+
+for (var i=0; i < taglist2.length; i++)
+{
+  tagStr2 = tagStr2 + " " + taglist2[i];
+}
+
+AddParty("keyword1", tagStr1);
+AddParty("keyword2", tagStr2);
+
+
+for (var j=0; j<tagAll.length;j++)
+{
+  AddTopic(tagAll[j]);
+}
+
+var r = d3.scale.sqrt()
+    .domain([0, d3.max(data.topics, function(d) { return d.count; })])
+    .range([0, maxRadius]);
+
+var force = d3.layout.force()
+    .charge(0)
+    .size([width, height - 80])
+    .on("tick", tick);
+
+var node = d3.select(".g-nodes").selectAll(".g-node"),
+    label = d3.select(".g-labels").selectAll(".g-label"),
+    arrow = d3.select(".g-nodes").selectAll(".g-note-arrow");
+
+d3.select(".g-nodes").append("rect")
+    .attr("class", "g-overlay")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", clear);
+
+d3.select(window)
+    .on("hashchange", hashchange);
+
+d3.select("#g-form")
+    .on("submit", submit);
+
+updateTopics(data.topics);
+hashchange();
+
+// Update the known topics.
+function updateTopics(topics) {
+  topics.forEach(function(d) {
+    d.r = r(d.count);
+    d.cr = Math.max(minRadius, d.r);
+    d.k = fraction(d.parties[0].count, d.parties[1].count);
+    if (isNaN(d.k)) d.k = .5;
+    if (isNaN(d.x)) d.x = (1 - d.k) * width + Math.random();
+    d.bias = .5 - Math.max(.1, Math.min(.9, d.k));
+  });
+  force.nodes(data.topics = topics).start();
+  updateNodes();
+  updateLabels();
+  tick({alpha: 0}); // synchronous update
+}
+
+// Returns the topic matching the specified name, approximately.
+// If no matching topic is found, returns undefined.
+function findTopic(name) {
+  for (var i = 0, n = data.topics.length, t; i < n; ++i) {
+    if ((t = data.topics[i]).name === name) {
+      return t;
+    }
+  }
+}
+
+// Returns the topic matching the specified name, approximately.
+// If no matching topic is found, a new one is created.
+function findOrAddTopic(name) {
+  var topic = findTopic(name);
+  if (!topic) {
+    topic = data.topic(name);
+    topic.y = 0;
+    updateTopics(data.topics);
+  }
+  return topic;
+}
+
+function AddTopic(name) {
+  topic = data.topic(name);
+  topic.y = 0;
+  topic.r = topic.count;
+  topic.cr = Math.max(minRadius, topic.r);
+  topic.k = fraction(topic.parties[0].count, topic.parties[1].count);
+    if (isNaN(topic.k)) topic.k = .5;
+    if (isNaN(topic.x)) topic.x = (1 - topic.k) * width + Math.random();
+    topic.bias = .5 - Math.max(.1, Math.min(.9, topic.k));
+  /*force.nodes(data.topics = topics).start();
+  updateNodes();
+  updateLabels();
+  tick({alpha: 0}); // synchronous update*/
+  return topic;
+}
+
+function AddParty(name, text){
+  party = data.party(name, text);
+  return party;
+}
+
+// Update the displayed nodes.
+function updateNodes() {
+  node = node.data(data.topics, function(d) { return d.name; });
+
+  node.exit().remove();
+
+  var nodeEnter = node.enter().append("a")
+      .attr("class", "g-node")
+      .attr("xlink:href", function(d) { return "#" + encodeURIComponent(d.name); })
+      .call(force.drag)
+      .call(linkTopic);
+
+  var democratEnter = nodeEnter.append("g")
+      .attr("class", "g-democrat");
+
+  democratEnter.append("clipPath")
+      .attr("id", function(d) { return "g-clip-democrat-" + d.id; })
+    .append("rect");
+
+  democratEnter.append("circle");
+
+  var republicanEnter = nodeEnter.append("g")
+      .attr("class", "g-republican");
+
+  republicanEnter.append("clipPath")
+      .attr("id", function(d) { return "g-clip-republican-" + d.id; })
+    .append("rect");
+
+  republicanEnter.append("circle");
+
+  nodeEnter.append("line")
+      .attr("class", "g-split");
+
+  node.selectAll("rect")
+      .attr("y", function(d) { return -d.r - clipPadding; })
+      .attr("height", function(d) { return 2 * d.r + 2 * clipPadding; });
+
+  node.select(".g-democrat rect")
+      .style("display", function(d) { return d.k > 0 ? null : "none" })
+      .attr("x", function(d) { return -d.r - clipPadding; })
+      .attr("width", function(d) { return 2 * d.r * d.k + clipPadding; });
+
+  node.select(".g-republican rect")
+      .style("display", function(d) { return d.k < 1 ? null : "none" })
+      .attr("x", function(d) { return -d.r + 2 * d.r * d.k; })
+      .attr("width", function(d) { return 2 * d.r; });
+
+  node.select(".g-democrat circle")
+      .attr("clip-path", function(d) { return d.k < 1 ? "url(#g-clip-democrat-" + d.id + ")" : null; });
+
+  node.select(".g-republican circle")
+      .attr("clip-path", function(d) { return d.k > 0 ? "url(#g-clip-republican-" + d.id + ")" : null; });
+
+  node.select(".g-split")
+      .attr("x1", function(d) { return -d.r + 2 * d.r * d.k; })
+      .attr("y1", function(d) { return -Math.sqrt(d.r * d.r - Math.pow(-d.r + 2 * d.r * d.k, 2)); })
+      .attr("x2", function(d) { return -d.r + 2 * d.r * d.k; })
+      .attr("y2", function(d) { return Math.sqrt(d.r * d.r - Math.pow(-d.r + 2 * d.r * d.k, 2)); });
+
+  node.selectAll("circle")
+      .attr("r", function(d) { return r(d.count); });
+}
+
+// Update the displayed node labels.
+function updateLabels() {
+  label = label.data(data.topics, function(d) { return d.name; });
+
+  label.exit().remove();
+
+  var labelEnter = label.enter().append("a")
+      .attr("class", "g-label")
+      .attr("href", function(d) { return "#" + encodeURIComponent(d.name); })
+      .call(force.drag)
+      .call(linkTopic);
+
+  labelEnter.append("div")
+      .attr("class", "g-name")
+      .text(function(d) { return d.name; });
+
+  labelEnter.append("div")
+      .attr("class", "g-value");
+
+  label
+      .style("font-size", function(d) { return Math.max(8, d.r / 2) + "px"; })
+      .style("width", function(d) { return d.r * 2.5 + "px"; });
+
+  // Create a temporary span to compute the true text width.
+  label.append("span")
+      .text(function(d) { return d.name; })
+      .each(function(d) { d.dx = Math.max(d.r * 2.5, this.getBoundingClientRect().width); })
+      .remove();
+
+  label
+      .style("width", function(d) { return d.dx + "px"; })
+    .select(".g-value")
+      .text(function(d) { return formatShortCount(d.parties[0].count) + " - " + formatShortCount(d.parties[1].count); });
+
+  // Compute the height of labels when wrapped.
+  label.each(function(d) { d.dy = this.getBoundingClientRect().height; });
+}
+
+// Assign event handlers to topic links.
+function linkTopic(a) {
+  a   .on("click", click)
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout);
+}
+
+// Simulate forces and update node and label positions on tick.
+function tick(e) {
+  node
+      .each(bias(e.alpha * 105))
+      .each(collide(.5))
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  label
+      .style("left", function(d) { return (d.x - d.dx / 2) + "px"; })
+      .style("top", function(d) { return (d.y - d.dy / 2) + "px"; });
+
+  arrow.style("stroke-opacity", function(d) {
+    var dx = d.x - d.cx, dy = d.y - d.cy;
+    return dx * dx + dy * dy < d.r * d.r ? 1: 0;
+  });
+}
+
+// A left-right bias causing topics to orient by party preference.
+function bias(alpha) {
+  return function(d) {
+    d.x += d.bias * alpha;
+  };
+}
+
+// Resolve collisions between nodes.
+function collide(alpha) {
+  var q = d3.geom.quadtree(data.topics);
+  return function(d) {
+    var r = d.cr + maxRadius + collisionPadding,
+        nx1 = d.x - r,
+        nx2 = d.x + r,
+        ny1 = d.y - r,
+        ny2 = d.y + r;
+    q.visit(function(quad, x1, y1, x2, y2) {
+      if (quad.point && (quad.point !== d) && d.other !== quad.point && d !== quad.point.other) {
+        var x = d.x - quad.point.x,
+            y = d.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = d.cr + quad.point.r + collisionPadding;
+        if (l < r) {
+          l = (l - r) / l * alpha;
+          d.x -= x *= l;
+          d.y -= y *= l;
+          quad.point.x += x;
+          quad.point.y += y;
+        }
+      }
+      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    });
+  };
+}
+
+// Fisher–Yates shuffle.
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+
+// Given two quantities a and b, returns the fraction to split the circle a + b.
+function fraction(a, b) {
+  var k = a / (a + b);
+  if (k > 0 && k < 1) {
+    var t0, t1 = Math.pow(12 * k * Math.PI, 1 / 3);
+    for (var i = 0; i < 10; ++i) { // Solve for theta numerically.
+      t0 = t1;
+      t1 = (Math.sin(t0) - t0 * Math.cos(t0) + 2 * k * Math.PI) / (1 - Math.cos(t0));
+    }
+    k = (1 - Math.cos(t1 / 2)) / 2;
+  }
+  return k;
+}
+
+// Update the active topic on hashchange, perhaps creating a new topic.
+function hashchange() {
+  var name = decodeURIComponent(location.hash.substring(1)).trim();
+  updateActiveTopic(name && name != "!" ? findOrAddTopic(name) : null);
+}
+
+// Trigger a hashchange on submit.
+function submit() {
+  var name = this.search.value.trim();
+  location.hash = name ? encodeURIComponent(name) : "!";
+  this.search.value = "";
+  d3.event.preventDefault();
+}
+
+// Clear the active topic when clicking on the chart background.
+function clear() {
+  location.replace("#!");
+}
+
+// Rather than flood the browser history, use location.replace.
+function click(d) {
+  location.replace("#" + encodeURIComponent(d === activeTopic ? "!" : d.name));
+  d3.event.preventDefault();
+}
+
+function mouseover(d) {
+  node.classed("g-hover", function(p) { return p === d; });
+}
+
+function mouseout(d) {
+  node.classed("g-hover", false);
+}
+
+})();
