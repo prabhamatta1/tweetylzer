e416bd92095d1a1a69d933e8417983ef073041c1
Prabha Matta <prabha@WhiteBook.local>
2012-11-10 16:58:36 -0800

    Integrate Bubble Visualization for Twitter tags

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+$(function() {
+
 var hash = []; //get the hashtags for each tweet
 var hash1 = []; //get the hashtags for each tweet
 //var tags;
@@ -16,10 +19,7 @@ var tagCommon = {};
 var query;
 var query1,query2;
 var tweets;
-var width = 960, height = 500;
-
-
-$(function() {
+var width = 960, height = 700;
 
     // =================================================================
     // FUNCTIONS FOR GETTING TWEETS
@@ -166,7 +166,12 @@ $(function() {
                     }
 
                     console.log(tagCommon);
-                    getMap(query1,tagFreq,query2,tagFreq1,tagCommon);
+                    console.log(hashtxt);
+                    console.log(hashtxt1);
+                    // getMap(query1,tagFreq,query2,tagFreq1,tagCommon);
+                    var hashAll=hashtxt.concat(hashtxt1)
+                    visualizeBubble(hashtxt,hashtxt1,hashAll);
+                    // hello();
 
 
                 } 
@@ -174,159 +179,6 @@ $(function() {
         });      
     }
 
-
-    function visualize()
-    {
-
-//prabha - can you work on creating a string using tags and counts , after which the function will display it in the grid format.
-//nodes - appear on the x and y axis
-//links will connect a source and target node with a specific color, here source = target with value = no of tags in common.
-     
-var jsontag = {"nodes":[{"name":"rohan","group":0},{"name":"haroon","group":1},{"name":"prabha","group":2},{"name":"tag1","group":3},{"name":"tag2","group":4},{"name":"tag3","group":5},{"name":"tag4","group":6},{"name":"tag5","group":7},{"name":"sayantan","group":8}],
-"links":[{"source":1,"target":1,"value":4},{"source":5,"target":5,"value":20},{"source":8,"target":8,"value":7}]};
-
-jQuery.parseJSON(jsontag);
-
-var margin = {top: 80, right: 0, bottom: 10, left: 80},
-    width = 400,
-    height = 400;
-
-var x = d3.scale.ordinal().rangeBands([0, width]),
-    z = d3.scale.linear().domain([0, 4]).clamp(true),
-    c = d3.scale.category10().domain(d3.range(10));
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style("margin-left", 100 + "px")
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-  var matrix = [],
-      nodes = jsontag.nodes,
-      n = nodes.length;
-
-  // Compute index per node.
-  nodes.forEach(function(node, i) {
-    node.index = i;
-    node.count = 0;
-    matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
-  });
-
-  // Convert links to matrix; count character occurrences.
-  jsontag.links.forEach(function(link) {
-    matrix[link.source][link.target].z += link.value;
-    //matrix[link.target][link.source].z += link.value;
-    //matrix[link.source][link.source].z += link.value;
-    //matrix[link.target][link.target].z += link.value;
-    //nodes[link.source].count += link.value;
-    nodes[link.target].count += link.value;
-  });
-
-  // Precompute the orders.
-  var orders = {
-    name: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].name, nodes[b].name); }),
-    count: d3.range(n).sort(function(a, b) { return nodes[b].count - nodes[a].count; }),
-    group: d3.range(n).sort(function(a, b) { return nodes[b].group - nodes[a].group; })
-  };
-
-  // The default sort order.
-  x.domain(orders.name);
-
-  
-  svg.append("rect")
-      .attr("class", "background")
-      .attr("width", width)
-      .attr("height", height);
-
-  var row = svg.selectAll(".row")
-      .data(matrix)
-    .enter().append("g")
-      .attr("class", "row")
-      .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
-      .each(row);
-
-  row.append("line")
-      .attr("x2", width);
-
-  row.append("text")
-      .attr("x", -6)
-      .attr("y", x.rangeBand() / 2)
-      .attr("dy", ".32em")
-      .attr("text-anchor", "end")
-      .text(function(d, i) { return nodes[i].name; });
-
-  var column = svg.selectAll(".column")
-      .data(matrix)
-    .enter().append("g")
-      .attr("class", "column")
-      .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-
-  column.append("line")
-      .attr("x1", -width);
-
-  column.append("text")
-      .attr("x", 6)
-      .attr("y", x.rangeBand() / 2)
-      .attr("dy", ".32em")
-      .attr("text-anchor", "start")
-      .text(function(d, i) { return nodes[i].name; });
-
-  function row(row) {
-    var cell = d3.select(this).selectAll(".cell")
-        .data(row.filter(function(d) { return d.z; }))
-      .enter().append("rect")
-        .attr("class", "cell")
-        .attr("x", function(d) { return x(d.x); })
-        .attr("width", x.rangeBand())
-        .attr("height", x.rangeBand())
-        .style("fill-opacity", function(d) { return z(d.z); })
-        .style("fill", function(d) { return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null; })
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout);
-  }
-
-  function mouseover(p) {
-    d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
-    d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
-  }
-
-  function mouseout() {
-    d3.selectAll("text").classed("active", false);
-  }
-
-  d3.select("#order").on("change", function() {
-    clearTimeout(timeout);
-    order(this.value);
-  
-
-  function order(value) {
-    x.domain(orders[value]);
-
-    var t = svg.transition().duration(2500);
-
-    t.selectAll(".row")
-        .delay(function(d, i) { return x(i) * 4; })
-        .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
-      .selectAll(".cell")
-        .delay(function(d) { return x(d.x) * 4; })
-        .attr("x", function(d) { return x(d.x); });
-
-    t.selectAll(".column")
-        .delay(function(d, i) { return x(i) * 4; })
-        .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-  }
-
-  var timeout = setTimeout(function() {
-    order("group");
-    d3.select("#order").property("selectedIndex", 2).node().focus();
-  }, 5000);
-});
-
-
-
-    }
 
     // =================================================================
     // EVENT FUNCTIONS
@@ -350,7 +202,7 @@ var svg = d3.select("body").append("svg")
         $('#tags1').empty();
         getTweets(1);
 
-        visualize();
+        visualizeSquare();
 
     });
 
@@ -361,6 +213,161 @@ var svg = d3.select("body").append("svg")
         var query = $(this).html();
         getTweets(query);
     });
+
+
+
+    function visualizeSquare()
+    {
+
+        //prabha - can you work on creating a string using tags and counts , after which the function will display it in the grid format.
+        //nodes - appear on the x and y axis
+        //links will connect a source and target node with a specific color, here source = target with value = no of tags in common.
+             
+        var jsontag = {"nodes":[{"name":"rohan","group":0},{"name":"haroon","group":1},{"name":"prabha","group":2},{"name":"tag1","group":3},{"name":"tag2","group":4},{"name":"tag3","group":5},{"name":"tag4","group":6},{"name":"tag5","group":7},{"name":"sayantan","group":8}],
+        "links":[{"source":1,"target":1,"value":4},{"source":5,"target":5,"value":20},{"source":8,"target":8,"value":7}]};
+
+        jQuery.parseJSON(jsontag);
+
+        var margin = {top: 80, right: 0, bottom: 10, left: 80},
+            width = 400,
+            height = 400;
+
+        var x = d3.scale.ordinal().rangeBands([0, width]),
+            z = d3.scale.linear().domain([0, 4]).clamp(true),
+            c = d3.scale.category10().domain(d3.range(10));
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .style("margin-left", 100 + "px")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+          var matrix = [],
+              nodes = jsontag.nodes,
+              n = nodes.length;
+
+          // Compute index per node.
+          nodes.forEach(function(node, i) {
+            node.index = i;
+            node.count = 0;
+            matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
+          });
+
+          // Convert links to matrix; count character occurrences.
+          jsontag.links.forEach(function(link) {
+            matrix[link.source][link.target].z += link.value;
+            //matrix[link.target][link.source].z += link.value;
+            //matrix[link.source][link.source].z += link.value;
+            //matrix[link.target][link.target].z += link.value;
+            //nodes[link.source].count += link.value;
+            nodes[link.target].count += link.value;
+          });
+
+          // Precompute the orders.
+          var orders = {
+            name: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].name, nodes[b].name); }),
+            count: d3.range(n).sort(function(a, b) { return nodes[b].count - nodes[a].count; }),
+            group: d3.range(n).sort(function(a, b) { return nodes[b].group - nodes[a].group; })
+          };
+
+          // The default sort order.
+          x.domain(orders.name);
+
+          
+          svg.append("rect")
+              .attr("class", "background")
+              .attr("width", width)
+              .attr("height", height);
+
+          var row = svg.selectAll(".row")
+              .data(matrix)
+            .enter().append("g")
+              .attr("class", "row")
+              .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+              .each(row);
+
+          row.append("line")
+              .attr("x2", width);
+
+          row.append("text")
+              .attr("x", -6)
+              .attr("y", x.rangeBand() / 2)
+              .attr("dy", ".32em")
+              .attr("text-anchor", "end")
+              .text(function(d, i) { return nodes[i].name; });
+
+          var column = svg.selectAll(".column")
+              .data(matrix)
+            .enter().append("g")
+              .attr("class", "column")
+              .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+
+          column.append("line")
+              .attr("x1", -width);
+
+          column.append("text")
+              .attr("x", 6)
+              .attr("y", x.rangeBand() / 2)
+              .attr("dy", ".32em")
+              .attr("text-anchor", "start")
+              .text(function(d, i) { return nodes[i].name; });
+
+          function row(row) {
+            var cell = d3.select(this).selectAll(".cell")
+                .data(row.filter(function(d) { return d.z; }))
+              .enter().append("rect")
+                .attr("class", "cell")
+                .attr("x", function(d) { return x(d.x); })
+                .attr("width", x.rangeBand())
+                .attr("height", x.rangeBand())
+                .style("fill-opacity", function(d) { return z(d.z); })
+                .style("fill", function(d) { return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null; })
+                .on("mouseover", mouseover)
+                .on("mouseout", mouseout);
+          }
+
+          function mouseover(p) {
+            d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
+            d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
+          }
+
+          function mouseout() {
+            d3.selectAll("text").classed("active", false);
+          }
+
+          d3.select("#order").on("change", function() {
+            clearTimeout(timeout);
+            order(this.value);
+          
+
+          function order(value) {
+            x.domain(orders[value]);
+
+            var t = svg.transition().duration(2500);
+
+            t.selectAll(".row")
+                .delay(function(d, i) { return x(i) * 4; })
+                .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+              .selectAll(".cell")
+                .delay(function(d) { return x(d.x) * 4; })
+                .attr("x", function(d) { return x(d.x); });
+
+            t.selectAll(".column")
+                .delay(function(d, i) { return x(i) * 4; })
+                .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+          }
+
+          var timeout = setTimeout(function() {
+            order("group");
+            d3.select("#order").property("selectedIndex", 2).node().focus();
+          }, 5000);
+        });
+
+
+
+    }
 
     // d3 visualization of tags
 
